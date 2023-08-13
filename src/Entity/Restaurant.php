24a16d624e0cb34a87ac93cb01cb1edfb0c91424
Gaetan <gaetan.role@gmail.com>
2023-08-13 13:40:16 +0200

    Creating Restaurant entity and its repository.

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 32)]
+    private string $name;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private string $description;
+
+    #[ORM\Column]
+    private array $amOpeningTime = [];
+
+    #[ORM\Column]
+    private array $pmOpeningTime = [];
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private int $maxGuest = 0;
+
+    #[ORM\Column]
+    private DateTimeImmutable $createdAt;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $updatedAt = null;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAmOpeningTime(): array
+    {
+        return $this->amOpeningTime;
+    }
+
+    public function setAmOpeningTime(array $amOpeningTime): static
+    {
+        $this->amOpeningTime = $amOpeningTime;
+
+        return $this;
+    }
+
+    public function getPmOpeningTime(): array
+    {
+        return $this->pmOpeningTime;
+    }
+
+    public function setPmOpeningTime(array $pmOpeningTime): static
+    {
+        $this->pmOpeningTime = $pmOpeningTime;
+
+        return $this;
+    }
+
+    public function getMaxGuest(): int
+    {
+        return $this->maxGuest;
+    }
+
+    public function setMaxGuest(int $maxGuest): static
+    {
+        $this->maxGuest = $maxGuest;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
