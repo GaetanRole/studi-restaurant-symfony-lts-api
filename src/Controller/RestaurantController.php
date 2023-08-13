68471abf28378d6bc512e6994b260b20c2842efd
Gaetan <gaetan.role@gmail.com>
2023-08-13 14:03:32 +0200

    Creating RestaurantController with MakerBundle.

---
+++ src/Controller/RestaurantController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestaurantController extends AbstractController
+{
+    #[Route('/restaurant', name: 'app_restaurant')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RestaurantController.php',
+        ]);
+    }
+}
