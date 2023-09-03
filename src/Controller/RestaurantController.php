6f4733f75ecce478509643c66225d3a3f9569360
Gaetan <gaetan.role@gmail.com>
2023-09-04 00:55:11 +0200

    Adding OpenAPI annotations on every routes.

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -6,6 +6,7 @@ use App\Entity\Restaurant;
 use App\Repository\RestaurantRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,6 +26,31 @@ class RestaurantController extends AbstractController
     }
 
     #[Route(methods: 'POST')]
+    /** @OA\Post(
+     *     path="/api/restaurant",
+     *     summary="Créer un restaurant",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Données du restaurant à créer",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="name", type="string", example="Nom du restaurant"),
+     *             @OA\Property(property="description", type="string", example="Description du restaurant")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Restaurant créé avec succès",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Nom du restaurant"),
+     *             @OA\Property(property="description", type="string", example="Description du restaurant"),
+     *             @OA\Property(property="createdAt", type="string", format="date-time")
+     *         )
+     *     )
+     * )
+     */
     public function new(Request $request): JsonResponse
     {
         $restaurant = $this->serializer->deserialize($request->getContent(), Restaurant::class, 'json');
@@ -43,6 +69,33 @@ class RestaurantController extends AbstractController
         return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
+    /** @OA\Get(
+     *     path="/api/restaurant/{id}",
+     *     summary="Afficher un restaurant par ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID du restaurant à afficher",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Restaurant trouvé avec succès",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Nom du restaurant"),
+     *             @OA\Property(property="description", type="string", example="Description du restaurant"),
+     *             @OA\Property(property="createdAt", type="string", format="date-time")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Restaurant non trouvé"
+     *     )
+     * )
+     */
     #[Route('/{id}', name: 'show', methods: 'GET')]
     public function show(int $id): JsonResponse
     {
@@ -56,6 +109,35 @@ class RestaurantController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
+    /** @OA\Put(
+     *     path="/api/restaurant/{id}",
+     *     summary="Modifier un restaurant par ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID du restaurant à modifier",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Nouvelles données du restaurant à mettre à jour",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="name", type="string", example="Nouveau nom du restaurant"),
+     *             @OA\Property(property="description", type="string", example="Nouvelle description du restaurant")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Restaurant modifié avec succès"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Restaurant non trouvé"
+     *     )
+     * )
+     */
     #[Route('/{id}', name: 'edit', methods: 'PUT')]
     public function edit(int $id, Request $request): JsonResponse
     {
@@ -77,6 +159,26 @@ class RestaurantController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
+    /** @OA\Delete(
+     *     path="/api/restaurant/{id}",
+     *     summary="Supprimer un restaurant par ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID du restaurant à supprimer",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Restaurant supprimé avec succès"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Restaurant non trouvé"
+     *     )
+     * )
+     */
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
     public function delete(int $id): JsonResponse
     {
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -20,6 +21,30 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/registration', name: 'registration', methods: 'POST')]
+    /** @OA\Post(
+     *     path="/api/registration",
+     *     summary="Inscription d'un nouvel utilisateur",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Données de l'utilisateur à inscrire",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="email", type="string", example="adresse@email.com"),
+     *             @OA\Property(property="password", type="string", example="Mot de passe")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Utilisateur inscrit avec succès",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="user", type="string", example="Nom d'utilisateur"),
+     *             @OA\Property(property="apiToken", type="string", example="31a023e212f116124a36af14ea0c1c3806eb9378"),
+     *             @OA\Property(property="roles", type="array", @OA\Items(type="string", example="ROLE_USER"))
+     *         )
+     *     )
+     * )
+     */
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
@@ -36,6 +61,30 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login', methods: 'POST')]
+    /** @OA\Post(
+     *     path="/api/login",
+     *     summary="Connecter un utilisateur",
+     *     @OA\RequestBody(
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  type="object",
+     *                  required={"username", "password"},
+     *                  @OA\Property(property="username", type="string"),
+     *                  @OA\Property(property="password", type="string")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Connexion réussie",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="X-AUTH-TOKEN", type="string")
+     *          )
+     *      )
+     * )
+     */
     public function login(#[CurrentUser] ?User $user): JsonResponse
     {
         if (null === $user) {
