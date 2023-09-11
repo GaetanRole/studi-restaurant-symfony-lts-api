e5aaaff643e207f4d44742090072b1cef261298b
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-09-11 19:35:31 +0200

    Add nelmio_cors

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
